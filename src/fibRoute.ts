5e30259dd9f510ba3b1cb1d62c1ecc042442a8e4
Chloe <Chloe.deng1000@gmail.com>
2023-01-23 09:49:54 -0500

    fix type errors in ribRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,8 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci = require("./fib");
 
-export default (req, res) => {
+export default (req: number, res: number) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
