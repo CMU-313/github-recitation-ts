a0cde6bbc709e6dca34d6e5d0f1311be7064f7c5
apoznanski <71227648+apoznanski@users.noreply.github.com>
2023-01-23 12:52:09 -0500

    translated fib.ts and fibRoute.ts from js to ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,9 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import { fibonacci } from "./fib";
+import {Request, Response} from 'express';
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
@@ -13,4 +14,4 @@ export default (req, res) => {
   }
 
   res.send(result);
-};
+}
