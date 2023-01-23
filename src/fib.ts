1176e40dae90ad2d0130e494d85eaf8034195928
liam-gersten <gerstenliam@gmail.com>
2023-01-23 13:10:26 -0500

    fib code change

--- src/fib.ts
+++ src/fib.ts
@@ -6,8 +6,8 @@ function fibonacci(n: number) : number {
     return 0;
   } else if (n == 1) {
     return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
-
-  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 export = fibonacci;
