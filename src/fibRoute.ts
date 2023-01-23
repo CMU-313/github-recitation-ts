8be875391fd95953b2724b238f2bc4612ac0fa14
Roshni_Surpur <rsurpur20@choate.edu>
2023-01-23 12:39:38 -0500

    fixed type errors

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,14 @@
-// Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+import express from 'express';
+import {Request, Response} from 'express';
 
-  const fibN = fibonacci(parseInt(num));
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  const {num} = req.params;
+
+  const fibN : number = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
@@ -13,4 +16,5 @@ export default (req, res) => {
   }
 
   res.send(result);
-};
+});
+
